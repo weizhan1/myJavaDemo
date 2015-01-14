b8f729dea71e1c47997c936888e155fa11c155c9
Zhang <wzhang@sjc-lpsz9.santaclara.corp.akamai.com>
2015-01-13 21:10:24 -0800

    First commit

---
+++ src/algoJava/demoJava.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package algoJava;
+
+/**
+ * @author wzhang
+ *
+ */
+public class demoJava {
+
+	/**
+	 * 
+	 */
+	public demoJava() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is a demo");
+	}
+
+}
---
+++ src/algoJava/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author wzhang
+ *
+ */
+package algoJava;
